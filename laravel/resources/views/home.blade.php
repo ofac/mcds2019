90c390d159f49c4dfeff7e7a733f22802f235aec
ofac <ofaczero@gmail.com>
2020-02-10 19:15:53 -0500

    Dashboard init

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -3,19 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+              <img src="{{ asset('imgs/users.png') }}" class="card-img-top" height="240px">
+              <div class="card-body">
+                <a href="{{ url('users') }}" class="btn btn-indigo btn-block">Módulo Usuarios</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+              <img src="{{ asset('imgs/categories.png') }}" class="card-img-top" height="240px">
+              <div class="card-body">
+                <a href="{{ url('categories') }}" class="btn btn-indigo btn-block">Módulo Categorías</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+              <img src="{{ asset('imgs/articles.png') }}" class="card-img-top" height="240px">
+              <div class="card-body">
+                <a href="{{ url('articles') }}" class="btn btn-indigo btn-block">Módulo Artículos</a>
+              </div>
             </div>
         </div>
     </div>
